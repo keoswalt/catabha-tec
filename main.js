2dc9309872c2fd2dd31923b35f232fe78eb8dede
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2024-12-13 09:58:00 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Mobile Navigation
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".nav-links");
+const navLink = document.querySelectorAll(".nav-link")
+const menuIcon = document.querySelector(".icon-hamburger");
+const closeIcon = document.querySelector(".icon-close");
+
+navToggle.addEventListener("click", () => {
+    const isVisible = primaryNav.hasAttribute("data-visible");
+    navToggle.setAttribute("aria-expanded", isVisible ? "false" : "true");
+    primaryNav.toggleAttribute("data-visible");
+    menuIcon.toggleAttribute("data-invisible");
+    closeIcon.toggleAttribute("data-visible");
+});
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        primaryNav.toggleAttribute("data-visible");
+        menuIcon.toggleAttribute("data-invisible");
+        closeIcon.toggleAttribute("data-visible");
+        const expanded = navToggle.getAttribute("aria-expanded") === 'true' || false;
+        navToggle.setAttribute("aria-expanded", !expanded);
+    });
+});
